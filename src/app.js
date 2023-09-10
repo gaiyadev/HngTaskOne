bfba431ac1bb0330e654b6fe89d02913774c11a3
Gaiya M. Obed <95748043+gaiyaobed@users.noreply.github.com>
2023-09-10 11:42:07 +0100

    first commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = 3000; // You can choose any port you prefer
+
+app.get("/api", (req, res) => {
+  const slackName = req.query.slack_name;
+  const track = req.query.track;
+
+  // Get the current date and time in UTC
+  const currentDate = new Date();
+  const currentDay = currentDate.toLocaleDateString("en-US", {
+    weekday: "long",
+  });
+  const utcTime = currentDate.toISOString();
+
+  // Replace these with your GitHub URLs
+  const githubFileUrl =
+    "https://github.com/username/repo/blob/main/file_name.ext";
+  const githubRepoUrl = "https://github.com/username/repo";
+
+  const responseJson = {
+    slack_name: slackName,
+    current_day: currentDay,
+    utc_time: utcTime,
+    track: track,
+    github_file_url: githubFileUrl,
+    github_repo_url: githubRepoUrl,
+    status_code: 200,
+  };
+
+  res.json(responseJson);
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
